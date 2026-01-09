07fa3ecf66d4fe0d051f7c47c626e9326d6eb35d
Eduarda <eduardapinheiros44@gmail.com>
2026-01-08 22:10:13 -0300

    added video data

---
+++ js/videos.js
@@ -0,0 +1,156 @@
+export const videos = [
+    //Growcast
+    {
+        img: "https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg",
+        title: "Growcast #00 | Conheça a Growdev",
+        link: "https://youtu.be/aJ-HZuLcKeA?si=wIfkDzFkeqkAer-u",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/7OWT3lfHYvE/sddefault.jpg",
+        title: "Growcast #01 | As profissões do futuro",
+        link: "https://youtu.be/7OWT3lfHYvE?si=w60oFXUAuwCK2B1n",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/ci1eQGPK89o/sddefault.jpg",
+        title: "Growcast #02 | Como seguir a carreira internacional?",
+        link: "https://youtu.be/ci1eQGPK89o?si=fH4fOrnb7EGqgo9A",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/WM7pGG7OicQ/sddefault.jpg",
+        title: "Growcast #03 | Superando os desafios da carreira de Dev!",
+        link: "https://youtu.be/WM7pGG7OicQ?si=5RV9Fud7t0-oh7S5",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/2c8VOBZ160Y/sddefault.jpg",
+        title: "Growcast #04 | Agile Master: tudo sobre a profissão!",
+        link: "https://youtu.be/2c8VOBZ160Y?si=WGqhD590Y65kMRgt",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/mxnEm03cYPc/sddefault.jpg",
+        title: "Growcast #5 | Como iniciar na carreira em Desenvolvimento de Softwares?",
+        link: "https://youtu.be/mxnEm03cYPc?si=IIXnz3xXA4zMHWi-",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/m7DyR07KrOE/sddefault.jpg",
+        title: "Growcast #06 | Mulheres e inclusão na tecnologia",
+        link: "https://youtu.be/m7DyR07KrOE?si=EZSjJa9ObDvf5hnG",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/vW8G9KPwRL0/sddefault.jpg",
+        title: "Growcast #07 | Desenvolvendo profissionais na área de TI",
+        link: "https://youtu.be/vW8G9KPwRL0?si=k9pyvcrcXrrMkF0K",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/_Ut2KqAqPyk/sddefault.jpg",
+        title: "Growcast #8 | A solução que está transformando o mercado de TI no Brasil",
+        link: "https://youtu.be/_Ut2KqAqPyk?si=fPXIYobkFJ-ZU2j9",
+        category: "Growcast",
+    },
+    {
+        img: "https://img.youtube.com/vi/dfDgs0Ive2Q/sddefault.jpg",
+        title: "Growcast #9 | Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI.",
+        link: "https://youtu.be/dfDgs0Ive2Q?si=AaeOfG6-DrcXXRpY",
+        category: "Growcast",
+    },
+
+    //Webinar em Flutter
+
+    {
+        img: "https://img.youtube.com/vi/WKbLXuP9S-c/sddefault.jpg",
+        title: "Webinar - Recriando a interface do Linkedin com Flutter",
+        link: "https://www.youtube.com/live/WKbLXuP9S-c?si=7sBRzY-0Jw4PSjZ1",
+        category: "Webinar em Flutter",
+    },
+    {
+        img: "https://img.youtube.com/vi/dJ6y1zMjVXU/sddefault.jpg",
+        title: "Webinar - Recriando a interface do Instagram com Flutter",
+        link: "https://www.youtube.com/live/dJ6y1zMjVXU?si=zvswsPB7PIv8ZqJV",
+        category: "Webinar em Flutter",
+    },
+    {
+        img: "https://img.youtube.com/vi/8rdJsqUjsGg/sddefault.jpg",
+        title: "Webinar - Recriando a interface do iFood com Flutter",
+        link: "https://www.youtube.com/live/8rdJsqUjsGg?si=pgzqec4tM13vB5-m",
+        category: "Webinar em Flutter",
+    },
+    {
+        img: "https://img.youtube.com/vi/L1pKoMC6hWk/sddefault.jpg",
+        title: "Webinar - Recriando o layout do WhatsApp com Flutter",
+        link: "https://www.youtube.com/live/L1pKoMC6hWk?si=xcUOHJRk5qud_9M-",
+        category: "Webinar em Flutter",
+    },
+
+    // Jornada UX/UI
+
+    {
+        img: "https://img.youtube.com/vi/FiZMowbaiJA/sddefault.jpg",
+        title: "Live #5 Jornada UX/UI: O UX/UI Design é para todos?",
+        link: "https://www.youtube.com/live/FiZMowbaiJA?si=m00yYR10DIBziVje",
+        category: "Jornada UX/UI",
+    },
+    {
+        img: "https://img.youtube.com/vi/cwqDu0ZzbYE/sddefault.jpg",
+        title: "Live #4 Jornada UX/UI: Como migrar para o UX/UI Design: a trajetória de uma designer - Growdev",
+        link: "https://www.youtube.com/live/cwqDu0ZzbYE?si=sUOIXy_fQoBLJvD6",
+        category: "Jornada UX/UI",
+    },
+    {
+        img: "https://img.youtube.com/vi/ZMZjQ9aRuIY/sddefault.jpg",
+        title: "Live #3 Jornada UX/UI: As oportunidades de estudar um programa de formação pagando após formado",
+        link: "https://www.youtube.com/live/ZMZjQ9aRuIY?si=mrGCl6JlsRwhKzxO",
+        category: "Jornada UX/UI",
+    },
+    {
+        img: "https://img.youtube.com/vi/8sd3wuKTZMA/sddefault.jpg",
+        title: "Live #2 Jornada UX/UI: O profissional de UX/UI Design no mercado digital - Growdev",
+        link: "https://www.youtube.com/live/8sd3wuKTZMA?si=rz5JkhxjfYdp-rdl",
+        category: "Jornada UX/UI",
+    },
+    {
+        img: "https://img.youtube.com/vi/-26aqPtgoEY/sddefault.jpg",
+        title: "Live #1 Jornada UX/UI: O papel do UX/UI Designer no desenvolvimento de um produto digital - Growdev",
+        link: "https://www.youtube.com/live/-26aqPtgoEY?si=mJHQpvLTX2ri7ycJ",
+        category: "Jornada UX/UI",
+    },
+
+    // Diversos
+
+    {
+        img: "https://img.youtube.com/vi/QsuhYgsSYQo/sddefault.jpg",
+        title: "Célula de talentos - Uma solução Growdev",
+        link: "https://youtu.be/QsuhYgsSYQo?si=w76hAls1PZqoxmB_",
+        category: "Diversos",
+    },
+    {
+        img: "https://img.youtube.com/vi/ouDn8z3PSqs/sddefault.jpg",
+        title: "Como é o dia a dia de um dev? Live Growdev com Marcelo Eltz",
+        link: "https://www.youtube.com/live/ouDn8z3PSqs?si=bswyFDmGTAa3tnXw",
+        category: "Diversos",
+    },
+    {
+        img: "https://img.youtube.com/vi/cQDQNX6NXYo/sddefault.jpg",
+        title: "Onde trabalhar com qualidade de software? Conheça a Testing Company, empresa parceira da Growdev",
+        link: "https://youtu.be/cQDQNX6NXYo?si=EGlaD-MRqbUGnNhR",
+        category: "Diversos",
+    },
+    {
+        img: "https://img.youtube.com/vi/nKGbolkirBM/sddefault.jpg",
+        title: "WEBINAR ANALISTA DE DADOS - com Paulo Marcon da Arezzo&Co",
+        link: "https://www.youtube.com/live/nKGbolkirBM?si=OqSFP6NM5TXek-sz",
+        category: "Diversos",
+    },
+    {
+        img: "https://img.youtube.com/vi/6sZPsqNjeek/sddefault.jpg",
+        title: "Tecnologia trazendo soluções para a Arezzo & CO | Ricardo Pinho",
+        link: "https://youtu.be/6sZPsqNjeek?si=qpyVly_IwjHi5_yV",
+        category: "Diversos",
+    }
+];
